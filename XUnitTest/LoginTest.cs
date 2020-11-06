1daa8adaebf22abe4ffd76130313855cf999075c
roderlol <39329480+roderlol@users.noreply.github.com>
2020-11-07 07:45:12 +0800

    Added unit testing for logins
    very simple test for logging in, only did 4 user login user stories so far

---
+++ XUnitTest/LoginTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BB6;
+
+
+namespace Testing
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void TestBugReporterLoginBugReporter()
+        {
+            //Arrange
+            string loginID = "benton";
+            string password = "benton123";
+            string userType = "BR";
+
+            UserClass benAccount = new UserClass(loginID, password, userType);
+
+            //Act
+
+
+            //Assert (https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert.istrue?view=mstest-net-1.3.2) Assert functions
+            Assert.IsTrue(benAccount.validateUser()); // Returns a Bool if the 3 details exists in the database
+        }
+
+        [TestMethod]
+        public void TestBugReporterLoginTriager()
+        {
+            //Arrange
+            string loginID = "Jazryll";
+            string password = "jaz123";
+            string userType = "T";
+
+            UserClass jazAccount = new UserClass(loginID, password, userType);
+
+            //Act
+
+
+            //Assert (https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert.istrue?view=mstest-net-1.3.2) Assert functions
+            Assert.IsTrue(jazAccount.validateUser()); // Returns a Bool if the 3 details exists in the database
+        }
+
+        [TestMethod]
+        public void TestBugReporterLoginReviewer()
+        {
+            //Arrange
+            string loginID = "Kamali";
+            string password = "kamali123";
+            string userType = "R";
+
+            UserClass kamaliAccount = new UserClass(loginID, password, userType);
+
+            //Act
+
+
+            //Assert (https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert.istrue?view=mstest-net-1.3.2) Assert functions
+            Assert.IsTrue(kamaliAccount.validateUser()); // Returns a Bool if the 3 details exists in the database
+        }
+
+        [TestMethod]
+        public void TestBugReporterLoginDeveloper()
+        {
+            //Arrange
+            string loginID = "Keith";
+            string password = "keith123";
+            string userType = "D";
+
+            UserClass keithAccount = new UserClass(loginID, password, userType);
+
+            //Act
+
+
+            //Assert (https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.testtools.unittesting.assert.istrue?view=mstest-net-1.3.2) Assert functions
+            Assert.IsTrue(keithAccount.validateUser()); // Returns a Bool if the 3 details exists in the database
+        }
+    }
+}
---
+++ XUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("XUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3956238d-2af7-4566-829e-4dbc6b781e98")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
